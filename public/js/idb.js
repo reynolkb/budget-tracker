f3b47ee2a70e9f6187ba874ce24a1777f4a7e0f9
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-12-05 12:08:28 -0800

    updating code

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,7 @@ const request = indexedDB.open('budget_tracker', 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
+    // save a reference to the database
     const db = event.target.result;
 
     // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
@@ -30,7 +30,7 @@ request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new budget and there's no internet connection
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_transaction'], 'readwrite');
